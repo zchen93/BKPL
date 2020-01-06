5989acb4c5d4aefcb4e492f13fd7a97e4e036187
zchen93 <tm602502@gmail.com>
2020-01-06 16:01:04 -0500

    updated CSVWriter, dual output in both console and log file

--- volRep.py
+++ volRep.py
@@ -1,10 +1,12 @@
 """
 Author:
 Fred Chen
-Hardware Delivery Team
+Hardware Engineer
+Communication Hardware Delivery
+Global Devices
 Landis+Gyr Technology Inc.
 Alpharetta, Georgia, USA
-DEC 2019
+JAN 2020
 Contact: fred.chen@landisgyr.com
 """
 
@@ -13,7 +15,7 @@ import _initPort
 import datetime
 import threading
 import time
-import numpy as np# pip install numpy
+#import numpy as np# pip install numpy
 import csv
 import sys
 
@@ -24,7 +26,9 @@ Redirecting console output as log files
 """
 RunConOutDir = str("./conout")
 RunConOutFileName = str("RunConOut_" + TimeStamp + ".log")
-sys.stdout = open(RunConOutDir+ '/' + RunConOutFileName,'w')
+RunConOutFile = str(RunConOutDir + '/' + RunConOutFileName)
+OpenFile_RunConOut = open(RunConOutFile, 'w')
+#sys.stdout = open(RunConOutDir + '/' + RunConOutFileName,'w')
 
 """
 Directing result file written directory
@@ -42,6 +46,19 @@ Count = 0
 #enableTransOperation = _commandLib.ccTransCurrent(_commandLib.connMgr(EstablishedPort, "open"))
 #print(enableTransOperation)
 
+"""
+CSV Writer:
+"""
+csvfile = open(ResOutDir + '/' + ResOutFileName, 'w', newline='')
+writer = csv.writer(csvfile, delimiter=';', quotechar="'") # I don't fully understand why do we define writer this way...
+# https://stackoverflow.com/questions/44073900/csv-writer-removes-quotes-and-adds-newline
+# https://pymotw.com/2/csv/
+writer.writerow(['SEP=,'])
+writer.writerow(['Date & Time, Voltage[V]'])
+
+"""
+Methods that do the work:
+"""
 def periodReadV(Interval):
 #def periodReadV(Interval, Threshold):
     # Interval Unit: seconds
@@ -55,46 +72,46 @@ def periodReadV(Interval):
             Result_V.append(VolRead)
             Result_T.append(time.strftime("%m/%d/%Y %H:%M:%S")) # Formatting time to make Excel calculating the time difference better.
             # NextCall += Interval
+            line = (time.strftime("%m/%d/%Y %H:%M:%S") + ',' + VolRead + ',,,,,,,')
             NextCall += Interval # Seconds
-            time.sleep(NextCall - time.time())
             Count += 1
-            print("Iteration: " + str(Count) + " | Timestamp: " + time.strftime("%m/%d/%Y %H:%M:%S") + " | Voltage: " + VolRead)
+            writer.writerow({line})
+
+            MSG_ConOut = str("Iteration: " + str(Count) + " | Timestamp: " + time.strftime("%m/%d/%Y %H:%M:%S") + " | Voltage: " + VolRead)
+            print(MSG_ConOut)
+            OpenFile_RunConOut.write(MSG_ConOut + "\n")
+
+            time.sleep(NextCall - time.time())
 
             if float(_commandLib.readV(_commandLib.connMgr(EstablishedPort, "open"))) <= 8.2:
                 break
+        MSG_Volow = str("Voltage Reached/Below Threshold.")
 
-        print("Voltage Reached/Below Threshold.")
+        print(MSG_Volow)
+        OpenFile_RunConOut.write(MSG_Volow + "\n")
+
         _commandLib.loadOff(_commandLib.connMgr(EstablishedPort, "open"))
 
     except KeyboardInterrupt:
-        print("Interrupted by user pressing 'CTRL+C'.")
+        MSG_KBItr = str("Interrupted by user pressing 'CTRL+C'.")
+        print(MSG_KBItr)
+        OpenFile_RunConOut.write(MSG_KBItr + "\n")
         _commandLib.loadOff(_commandLib.connMgr(EstablishedPort, "open"))
 
 #Thread_1 = threading.Thread(target=periodReadV(VoltageQueryInterval))
 #Thread_1.start()
-
-periodReadV(120)
-
-"""
-CSV Writer:
-"""
-csvfile = open(ResOutDir + '/' + ResOutFileName, 'w', newline='')
-try:
-    writer = csv.writer(csvfile, delimiter=';', quotechar="'") # I don't fully understand why do we define writer this way...
-    # https://stackoverflow.com/questions/44073900/csv-writer-removes-quotes-and-adds-newline
-    # https://pymotw.com/2/csv/
-    writer.writerow(['SEP=,'])
-    writer.writerow(['Date & Time, Voltage[V]'])
-
+def outro():
     if len(Result_V) > 0:
-        for i in range(len(Result_V)):
-            line = (Result_T[i] + ',' + Result_V[i] + ',,,,,,,')
-            #print(line)
-            writer.writerow({line})
-        print("Result writing complete.")
+        MSG_Completion = str("Result writing complete. Readings: " + str(len(Result_V)))
+        print(MSG_Completion)
+        OpenFile_RunConOut.write(MSG_Completion + "\n")
     else:
-        print("Empty result detected. ")
-    _commandLib.loadOff(_commandLib.connMgr(EstablishedPort, "open"))
-finally:
+        MSG_EptyRes = str("Empty result detected.")
+        print(MSG_EptyRes)
+        OpenFile_RunConOut.write(MSG_EptyRes + "\n")
+
     csvfile.close()
     _commandLib.loadOff(_commandLib.connMgr(EstablishedPort, "open"))
+
+periodReadV(3)
+outro()
